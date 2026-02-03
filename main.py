ab623e3f52be0bd6607c87a793f78b8b3e9889a4
Omar Abo Hamza <www.omar.ha21@gmail.com>
2026-02-03 14:40:11 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
-app.middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
